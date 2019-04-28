02a91b12f2d78aa98f8355d900e703f0ab6a90e8
masterUNG <phrombutr@gmail.com>
2019-04-28 10:54:14 +0700

    Create Center and Marker Success

--- app/src/main/java/masterung/th/in/androidthai/demomapgun/MapsActivity.java
+++ app/src/main/java/masterung/th/in/androidthai/demomapgun/MapsActivity.java
@@ -13,6 +13,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private double centerLat = 13.674290, centerLng = 100.606693;   // Section Bangna
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,22 +26,30 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+//        Setup Center Map
+        LatLng centerLatLng = new LatLng(centerLat, centerLng);
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(centerLatLng, 16));
+
+        myCreateMarker(centerLat, centerLng);
+
+    }   // onMap
+
+    private void myCreateMarker(double lat, double lng) {
+
+        LatLng latLng = new LatLng(lat, lng);
+
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(latLng);
+
+        mMap.addMarker(markerOptions);
+
+
     }
+
+
 }
